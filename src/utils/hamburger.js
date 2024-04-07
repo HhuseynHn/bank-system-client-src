36d2625a18526453dd2bd122ffc82814301f86c6
Dell Latitude E5570 <hhuseyn573@gmail.com>
2024-04-07 23:38:14 +0400

    Bank sistem update responsive

--- src/utils/hamburger.js
+++ src/utils/hamburger.js
@@ -3,20 +3,22 @@
 export function hamburger() {
   let hambgerButton = document.querySelector(".hambr-icon");
     let navBar = document.querySelector(".nav-bar-list");
-    let closeIcon = document.querySelector(".close-icon");
+  let closeIcon = document.querySelector(".close-icon");
+  let main = document.querySelector(".main");
+  let balance = document.querySelector(".my-balance");
 
   hambgerButton.addEventListener("click", () => {
-      navBar.classList.toggle("navbar-list-show");
-      hambgerButton.classList.toggle("hidden")
-      closeIcon.classList.toggle("hidden")
-
+    navBar.classList.toggle("navbar-list-show");
+    hambgerButton.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
+    main.classList.toggle("hidden");
+    balance.classList.toggle("hidden");
   });
-    closeIcon.addEventListener("click", () => {
-        
-        navBar.classList.toggle("navbar-list-show");
-        hambgerButton.classList.toggle("hidden")
-        closeIcon.classList.toggle("hidden")
-
-
-    })
+  closeIcon.addEventListener("click", () => {
+    navBar.classList.toggle("navbar-list-show");
+    hambgerButton.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
+    main.classList.toggle("hidden");
+    balance.classList.toggle("hidden");
+  });
 }
